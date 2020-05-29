862ac15be804f3f1540466cb0cbefcd874d844a9
18301494080@163.com <l7057287>
2020-05-29 11:19:23 +0800

    123

--- src/main/java/com/zte/clonedata/job/JobDouban.java
+++ src/main/java/com/zte/clonedata/job/JobDouban.java
@@ -1,10 +1,6 @@
 package com.zte.clonedata.job;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.DefaultJSONParser;
-import com.alibaba.fastjson.parser.JSONLexer;
-import com.alibaba.fastjson.parser.ParserConfig;
 import com.zte.clonedata.Contanst;
 import com.zte.clonedata.dao.DoubanRepository;
 import com.zte.clonedata.model.Douban;
@@ -12,22 +8,12 @@ import com.zte.clonedata.util.HttpUtils;
 import com.zte.clonedata.util.JSONUtils;
 import com.zte.clonedata.util.PicDownUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -46,11 +32,10 @@ public class JobDouban{
 
     @Autowired
     private DoubanRepository doubanRepository;
-    private static String resultMD5 = "";
 
     /**
      */
-    @Scheduled(cron = "0 9 * * * ?")
+    @Scheduled(cron = "0 16 * * * ?")
     public void execute(){
         PicDownUtils picDownUtils = new PicDownUtils();
         int i = 0;
@@ -64,12 +49,6 @@ public class JobDouban{
             String data = JSONObject.parseObject(result, Map.class).get(Contanst.JSON_KEY_DOUBAN).toString();
             if (StringUtils.isEmpty(data) || data.length() <10){
                 break;
-            }
-            String MD5 = DigestUtils.md5DigestAsHex(data.getBytes());
-            if (resultMD5.equals(MD5)){
-                break;
-            }else {
-                resultMD5 = MD5;
             }
             i = i+1000;
             List<Douban> doubans = JSONUtils.parseArray(data, Douban.class);
